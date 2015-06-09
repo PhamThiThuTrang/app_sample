36d72668b8d793642ccb97ba94c90a77e1f8eb6c
Pham Thi Thu Trang <pham.thi.thu.trang@framgia.com>
2015-06-09 13:48:24 +0700

    add password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,15 @@
 class UserMailer < ApplicationMailer
 
+  # Activated account
   def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  # Reset Password
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
 
-  get 'users/new'
 
   root             'static_page#home'
   get 'help'    => 'static_page#help'
@@ -16,6 +19,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
